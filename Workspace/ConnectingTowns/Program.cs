0daef05cbf10a67b93ebffc08c283a5e9bd3d548
rudiger braun <rbraun@preventice.com>
2015-02-17 10:27:05 -0600

    ConnectingTowns solution

---
+++ Workspace/ConnectingTowns/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectingTowns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int noOfTestCases = Int32.Parse(Console.ReadLine().Trim());
+            long[] results = new long[noOfTestCases];
+            for (int i = 0; i < noOfTestCases; i++)
+            {
+                var noOfTowns = Convert.ToInt64(Console.ReadLine());
+                var routeLengthsAsString = Convert.ToString(Console.ReadLine()).Split(' ');
+                results[i] = ComputeDistance(routeLengthsAsString);
+            }
+            for(int j = 0; j<results.Length; j++)
+            {
+              Console.WriteLine(results[j]);
+            }
+            Console.ReadLine();
+        }
+        private static long ComputeDistance(string[] routeLengths)
+        {
+            long result = 0;
+            for (int j = 0; j < routeLengths.Length; j++)
+            {
+                    if (result == 0)
+                    {
+                        result = Int64.Parse(routeLengths[j]);
+                    }
+
+                    else
+                    {
+                        result *= Int64.Parse(routeLengths[j]);
+                        result = result % 1234567;
+                    }
+            }
+            return result;
+        }
+    }
+
+}

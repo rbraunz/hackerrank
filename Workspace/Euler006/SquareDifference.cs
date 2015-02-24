c9fad9c7c0091f6bf3865d2083d1f81bd734ecb3
rudiger braun <rbraun@preventice.com>
2015-02-24 14:46:09 -0600

    sum of squares solution

--- Workspace/Euler006/SquareDifference.cs
+++ Workspace/Euler006/SquareDifference.cs
@@ -10,6 +10,30 @@ namespace Euler006
     {
         static void Main(string[] args)
         {
+            ulong size = Convert.ToUInt64(Console.ReadLine());
+            ulong[] incomingLongs = new ulong[size];
+            for (ulong i = 0; i < size; i++)
+            {
+                var input = Convert.ToUInt64(Console.ReadLine());
+                incomingLongs[i] = input;
+            }
+            foreach (var n in incomingLongs)
+            {
+                ulong result = SumOfSquares(n) * SumOfSquares(n) - SumOfNaturalNumbers(n);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static ulong SumOfSquares(ulong n)
+        {
+            ulong result = n*(n + 1)/2;
+            return result;
+        }
+
+        private static ulong SumOfNaturalNumbers(ulong n)
+        {
+            ulong result = (n*(n + 1)*(2*n + 1))/6;
+            return result;
         }
     }
 }

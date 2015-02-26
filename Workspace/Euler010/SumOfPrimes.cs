c689095be77326a8712e5bc2064b3ca34a614e67
rudiger braun <rbraunz@gmail.com>
2015-02-26 17:16:37 -0600

    work on euler10

--- Workspace/Euler010/SumOfPrimes.cs
+++ Workspace/Euler010/SumOfPrimes.cs
@@ -18,46 +18,69 @@ namespace Euler010
                 var input = Convert.ToInt64(Console.ReadLine());
                 incomingLongs[i] = input;
             }
-            int maxprime = 1000000;
-            ArrayList primelist = sieve(maxprime);
             foreach (var n in incomingLongs)
             {
-                                Console.WriteLine();
+                var totalsum = new Atkin(n);
+                totalsum.FindPrimes();
+                Console.WriteLine(totalsum.sum);
+                //Console.WriteLine(GeneratePrimesParallel(n).Sum());
+                //Console.WriteLine(ESieve(n));
                 //Console.WriteLine("Count = " + primelist.Count);
             }
-            Console.ReadLine();
         }
 
+  
 
 
-        static ArrayList sieve(int arg_max_prime)
+    }
+    public class Atkin
+    {
+        private readonly long limit;
+        public long sum = 0;
+
+        public Atkin(long limit)
         {
-            BitArray al = new BitArray(arg_max_prime, true);
+            this.limit = limit;
+        }
 
-            int lastprime = 1;
-            int lastprimeSquare = 1;
-
-            while (lastprimeSquare <= arg_max_prime)
+        public void FindPrimes()
+        {
+            if (limit == 2)
             {
-                lastprime++;
-
-                while (!(bool)al[lastprime])
-                    lastprime++;
-
-                lastprimeSquare = lastprime * lastprime;
-
-                for (int i = lastprimeSquare; i < arg_max_prime; i += lastprime)
-                    if (i > 0)
-                        al[i] = false;
+                sum = 2;
+                return;
             }
+            if (limit == 3)
+            {
+                sum = 5;
+                return;
+            }
+            sum = 5;
+            var isPrime = new bool[limit + 1];
+            var sqrt = Math.Sqrt(limit);
+            for (long x = 1; x <= sqrt; x++)
+                for (long y = 1; y <= sqrt; y++)
+                {
+                    var n = 4 * x * x + y * y;
+                    if (n <= limit && (n%12 == 1 || n%12 == 5))
+                    {
+                        isPrime[n] ^= true;
+                        sum = sum + n;
+                    }
 
-            ArrayList sieve_2_return = new ArrayList();
-
-            for (int i = 2; i < arg_max_prime; i++)
-                if (al[i])
-                    sieve_2_return.Add(i);
-
-            return sieve_2_return;
+                    n = 3 * x * x + y * y;
+                    if (n <= limit && n%12 == 7)
+                    {
+                        isPrime[n] ^= true;
+                        sum = sum + n;
+                    }
+                    n = 3 * x * x - y * y;
+                    if (x > y && n <= limit && n%12 == 11)
+                    {
+                        isPrime[n] ^= true;
+                        sum = sum + n;
+                    }
+                }
         }
     }
 }
